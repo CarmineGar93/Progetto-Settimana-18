9865de3cd342e0db3323733535ff55e6152b54c1
Carmine Gargiulo <carmygargiulo45@gmail.com>
2024-10-31 16:28:10 +0100

    created entity employee

---
+++ src/main/java/CarmineGargiulo/Progetto_Settimana_18/entities/Employee.java
@@ -0,0 +1,43 @@
+package CarmineGargiulo.Progetto_Settimana_18.entities;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue
+    @Column(name = "employee_id")
+    @Setter(AccessLevel.NONE)
+    private UUID employeeId;
+    @Column(nullable = false)
+    private String username, name, surname, email;
+    @Column(name = "avatar_url", nullable = false)
+    private String avatarUrl;
+
+    public Employee(String username, String name, String surname, String email) {
+        this.username = username;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.avatarUrl = "https://ui-avatars.com/api/?name=" + name + "+" + surname;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee " + employeeId +
+                " = name: " + name +
+                ", surname: " + surname +
+                ", username: " + username +
+                ", email: " + email;
+    }
+}
