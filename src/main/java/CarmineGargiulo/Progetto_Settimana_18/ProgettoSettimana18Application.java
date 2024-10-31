53084a93adb6dd3dc809948c4d7900888759b8f1
Carmine Gargiulo <carmygargiulo45@gmail.com>
2024-10-31 15:07:29 +0100

    Initial commit

---
+++ src/main/java/CarmineGargiulo/Progetto_Settimana_18/ProgettoSettimana18Application.java
@@ -0,0 +1,13 @@
+package CarmineGargiulo.Progetto_Settimana_18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoSettimana18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoSettimana18Application.class, args);
+	}
+
+}
