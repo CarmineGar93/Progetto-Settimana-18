41350a4c12729668aead79d582ee10a4feaf8eb9
Carmine Gargiulo <carmygargiulo45@gmail.com>
2024-10-31 16:51:37 +0100

    created repositories

---
+++ src/main/java/CarmineGargiulo/Progetto_Settimana_18/repositories/EmployeesRepository.java
@@ -0,0 +1,11 @@
+package CarmineGargiulo.Progetto_Settimana_18.repositories;
+
+import CarmineGargiulo.Progetto_Settimana_18.entities.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface EmployeesRepository extends JpaRepository<Employee, UUID> {
+}
